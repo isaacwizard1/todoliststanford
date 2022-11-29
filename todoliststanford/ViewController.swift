4fdc6ab0f4ed6b89d666dd33d21d5710fe44db24
isaacwizard1 <106562755+isaacwizard1@users.noreply.github.com>
2022-11-29 14:16:08 -0500

    Initial Commit

---
+++ todoliststanford/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoliststanford
+//
+//  Created by isaac on 11/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
