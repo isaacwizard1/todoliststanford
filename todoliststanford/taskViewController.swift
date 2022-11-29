34f9d0758ed9fd1870fa48e1bdb08cda6337a504
isaacwizard1 <106562755+isaacwizard1@users.noreply.github.com>
2022-11-29 17:15:37 -0500

    to do list

---
+++ todoliststanford/taskViewController.swift
@@ -0,0 +1,20 @@
+//
+//  taskViewController.swift
+//  todoliststanford
+//
+//  Created by isaac on 11/29/22.
+//
+
+import UIKit
+
+class taskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
